b83fa42a10cfdd066e0a28b539cdd99787461fa5
Believe Kwamitse <believeserwaa80@gmail.com>
2024-09-17 15:48:39 +0000

    added values

--- scripts/register.js
+++ scripts/register.js
@@ -1,7 +1,26 @@
 // Get form element by id
-const registerForm = document.getElementById('register'); 
+const registerForm = document.querySelector('#register'); 
+console.log(registerForm);
 // Handle form submit event
-registerForm.onsubmit = function(event) {
+registerForm.addEventListener('submit', function (event) {
     event.preventDefault();
-    console.log(event);
-} 
+   //Collect form data
+   const formData = new FormData(registerForm); 
+   
+    //Send data to backend
+    //Display success message to user
+    const messageH1= document.querySelector('#message');
+    messageH1.textContent = 'Account registered successfully';
+    
+
+});
+// Declare variable age
+const age = 71;
+//Define square age function
+function squareAge(age) {
+       return age ** 2
+}
+
+const squaredAge = squareAge(34);
+console.log(squaredAge);
+
